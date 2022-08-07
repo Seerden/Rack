0e6ea435a24285bc7a6ee3a62748eb9ab65a73c1
Christiaan <christiaanseerden@live.nl>
2022-08-07 22:23:00 +0200

    (tweak) validate type of keys of newUser in isValidNewUser

--- client/src/components/Register/helpers/validate.ts
+++ client/src/components/Register/helpers/validate.ts
@@ -6,11 +6,21 @@ import { NewUser } from "../types/new-user.types";
  * - contains a `username`, `password` and `repeatPassword`
  * - `password` and `repeatPassword` have to be equal
  */
-export function isValidNewUser({ username, password, repeatPassword }: NewUser) {
+export function isValidNewUser(newUser: NewUser) {
+	const { username, password, repeatPassword } = newUser;
+
 	return (
+		Object.keys(newUser).every((key) => isKeyofNewUser(key)) &&
 		username?.length &&
 		password?.length &&
 		repeatPassword?.length &&
 		password === repeatPassword
 	);
+}
+
+/** Verify that a string is a key of NewUser. */
+export function isKeyofNewUser(s: string): s is keyof NewUser {
+	const keys = ["username", "password", "repeatPassword"];
+
+	return keys.includes(s);
 }
