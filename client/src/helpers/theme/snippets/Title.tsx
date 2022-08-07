1cbdfbb4610db4f715249cdcfb39879a7762b61c
Christiaan <christiaanseerden@live.nl>
2022-08-07 22:17:51 +0200

    (style) implement Title styled component

---
+++ client/src/helpers/theme/snippets/Title.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+	font-size: 1.5rem;
+	margin-bottom: 1.2rem;
+	font-weight: 600;
+
+	padding: 0 2rem 0.5rem 0;
+
+	border-bottom: 5px solid #eee;
+	max-width: max-content;
+`;
