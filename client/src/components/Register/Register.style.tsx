0dd4586d0837e672e60396972174f1b3b8f64872
Christiaan <christiaanseerden@live.nl>
2022-08-07 22:25:34 +0200

    (feat) set up Register form with styled elements and event handlers

---
+++ client/src/components/Register/Register.style.tsx
@@ -0,0 +1,72 @@
+import styled, { css } from "styled-components";
+import { inputStyle } from "../../helpers/theme/snippets/input";
+import { pageStyle } from "../../helpers/theme/snippets/page";
+import { inputLabelStyle } from "../../helpers/theme/snippets/text";
+
+const elementWidth = "200px";
+const boxShadow = css`
+	box-shadow: 0 0.4rem 0.5rem -0.15rem #a5a5a5;
+`;
+
+export const Form = styled.form`
+	${pageStyle};
+`;
+
+export const Label = styled.label`
+	display: flex;
+	flex-direction: column;
+
+	${inputLabelStyle};
+`;
+
+export const Input = styled.input`
+	width: ${elementWidth};
+
+	${inputStyle};
+
+	&:focus,
+	&:active {
+		${boxShadow};
+		transform: translateY(-2px);
+	}
+`;
+
+export const Button = styled.button`
+	width: ${elementWidth};
+
+	margin-top: 1rem;
+
+	display: flex;
+	justify-content: center;
+	padding: 0.7rem 1.5rem;
+
+	outline: none;
+	border: 2px solid #bbb;
+	background-color: inherit;
+
+	font-size: 0.94rem;
+	font-weight: 500;
+
+	&:disabled {
+		color: #bbb;
+	}
+
+	&:not(:disabled) {
+		border-color: #aaa;
+		box-shadow: 0 0 0.2rem 0 #eee;
+		border-radius: 4px;
+
+		&:hover,
+		&:active,
+		&:focus {
+			${boxShadow}
+			transform: translateY(-4px);
+		}
+
+		&:focus {
+			outline: 1px solid black;
+		}
+	}
+
+	transition: all 45ms ease-out;
+`;
--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,3 +1,47 @@
+import { Title } from "../../helpers/theme/snippets/Title";
+import { isValidNewUser } from "./helpers/validate";
+import useRegister from "./hooks/useRegister";
+import * as S from "./Register.style";
+
 export default function Register() {
-	return <></>;
+	const { handleChange, handleSubmit, newUser, handlePasswordBlur, message } =
+		useRegister();
+
+	return (
+		<S.Form>
+			<Title>Register an account</Title>
+
+			<S.Label htmlFor="username">Username:</S.Label>
+			<S.Input name="username" type="text" onChange={(e) => handleChange(e)} />
+
+			<S.Label htmlFor="password">Password:</S.Label>
+			<S.Input
+				name="password"
+				type="password"
+				onChange={(e) => handleChange(e)}
+				onBlur={() => handlePasswordBlur()}
+			/>
+
+			<S.Label htmlFor="repeatPassword">Repeat password:</S.Label>
+			<S.Input
+				name="repeatPassword"
+				type="password"
+				onChange={(e) => handleChange(e)}
+				onBlur={() => handlePasswordBlur()}
+			/>
+
+			<S.Button
+				type="submit"
+				onClick={(e) => {
+					e.preventDefault();
+					handleSubmit();
+				}}
+				disabled={!isValidNewUser(newUser)}
+			>
+				{!isValidNewUser(newUser) ? "Fill in all fields" : "Register"}
+			</S.Button>
+
+			{message && <aside>{message}</aside>}
+		</S.Form>
+	);
 }
