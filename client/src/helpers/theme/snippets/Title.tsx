0a011cfb2f2cf1a673af86aa4a1903ad81e5720f
Christiaan <christiaanseerden@live.nl>
2022-08-08 22:45:11 +0200

    (style) tweak Title element; implement SubTitle element

--- client/src/helpers/theme/snippets/Title.tsx
+++ client/src/helpers/theme/snippets/Title.tsx
@@ -5,8 +5,23 @@ export const Title = styled.h1`
 	margin-bottom: 1.2rem;
 	font-weight: 600;
 
-	padding: 0 2rem 0.5rem 0;
+	padding: 0.4rem 0.8rem;
+	border-radius: 5px;
 
 	border-bottom: 5px solid #eee;
+
+	background-color: #eee;
+	max-width: max-content;
+`;
+
+// Slightly 'lesser' version of Title
+export const SubTitle = styled.h2`
+	font-size: 1.35rem;
+	margin-bottom: 0.9rem;
+	font-weight: 600;
+
+	padding: 0 1.8rem 0.55rem 0.35rem;
+
+	border-bottom: 4px solid #eee;
 	max-width: max-content;
 `;
