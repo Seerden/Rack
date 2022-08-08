f69341ac6acb65b2caca9a9c91f7cd9f6c67dd29
Christiaan <christiaanseerden@live.nl>
2022-08-08 23:41:15 +0200

    (feat) use newWorkoutState in useNewWorkout and ipmlement onChange handler for exercise-related fields

--- client/src/components/Workout/NewWorkout/hooks/useNewWorkout.tsx
+++ client/src/components/Workout/NewWorkout/hooks/useNewWorkout.tsx
@@ -1,22 +1,55 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
+import { useRecoilState } from "recoil";
+import { splitNameAndIndex } from "../helpers/field-with-index";
+import { newWorkoutState } from "../state/new-workout-state";
 import NewExercise from "../sub/NewExercise";
 
 export default function useNewWorkout() {
+	const [newWorkout, setNewWorkout] = useRecoilState(newWorkoutState);
+
+	/** Reducer to manipulate `elements` state. Note that this has to be defined
+	 * inside this component so that it has access to local state setters. */
 	function fieldsetElementReducer(state: JSX.Element[], action: string): JSX.Element[] {
 		const rowCount = state.length;
 
 		switch (action) {
 			case "add":
-				return state.concat(<NewExercise key={rowCount} index={rowCount} />);
-
+				return state.concat(
+					<NewExercise
+						key={rowCount}
+						index={rowCount}
+						onChange={handleInputChange}
+					/>
+				);
 			default:
 				return state;
 		}
 	}
 
 	const [elements, dispatch] = useReducer(fieldsetElementReducer, [
-		<NewExercise key={0} index={0} />,
+		<NewExercise key={0} index={0} onChange={handleInputChange} />,
 	]);
 
-	return { elements, dispatch } as const;
+	useEffect(() => {
+		console.log({ newWorkout });
+	}, [newWorkout]);
+
+	function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const { name, value } = e.currentTarget;
+
+		const [field, exerciseIndex] = splitNameAndIndex(name);
+
+		// If true, we're changing an exercise field, not a workout field.
+		if (typeof exerciseIndex === "number") {
+			setNewWorkout((cur) => {
+				const exercises = structuredClone(cur.exercises);
+
+				exercises[exerciseIndex] = { ...exercises[exerciseIndex], [field]: value };
+
+				return { ...cur, exercises };
+			});
+		}
+	}
+
+	return { elements, dispatch, handleInputChange } as const;
 }
