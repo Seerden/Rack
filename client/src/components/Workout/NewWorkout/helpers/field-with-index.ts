7e2a7158b6836c017c7427948bce9cae035def8d
Christiaan <christiaanseerden@live.nl>
2022-08-08 22:30:06 +0200

    (doc) add docstrings to field-with-index helpers

--- client/src/components/Workout/NewWorkout/helpers/field-with-index.ts
+++ client/src/components/Workout/NewWorkout/helpers/field-with-index.ts
@@ -1,7 +1,15 @@
+/** Append `index` to a string
+ *
+ * @usage Forms with multiple rows of the same thing (e.g. NewWorkout, which
+ * contains multiple NewExercise input combinations) need each entry (e.g.
+ * NewExercise) to have its own id/name/htmlFor combinations. One way to achieve
+ * this is by associating an index with every entry.
+ */
 function stringWithIndex(s: string, index: number) {
 	return `${s}-${index}`;
 }
 
+/** Create a `stringWithIndex` function with pre-specified `index` parameter. */
 export function stringWithIndexFunctor(index: number) {
 	return (s: string) => stringWithIndex(s, index);
 }
