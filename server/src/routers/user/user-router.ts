1e01faec348a5c5ed1e2194bf7a6bdbaf389fbe4
Christiaan <christiaanseerden@live.nl>
2022-08-08 12:22:42 +0200

    (fix) return res.* to prevent rest of code being executed

--- server/src/routers/user/user-router.ts
+++ server/src/routers/user/user-router.ts
@@ -9,17 +9,17 @@ userRouter.post("/register", async (req, res) => {
    const newUser: any = req.body.newUser;
 
    if (!isNewUser(newUser)) {
-      res.status(400).json({ message: "Invalid `newUser` in request body." });
+      return res.status(400).json({ message: "Invalid `newUser` in request body." });
    }
 
    // This check is also done inside `insertUser`, but this way it's easiest to
    // send the correct response type without complicating `insertUser`
    if (await userExists({ username: newUser.username }))
-      res.status(403).json({ message: "Username already taken." });
+      return res.status(403).json({ message: "Username already taken." });
 
    const insertedUser = await insertUser({
       userInput: await parseNewUserForDatabase(newUser),
    });
 
-   res.status(201).json({ user: insertedUser });
+   return res.status(201).json({ user: insertedUser });
 });
