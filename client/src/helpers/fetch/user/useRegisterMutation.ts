1d92f4642dfa49b0013dc550ebd7b98ec21d309a
Christiaan <christiaanseerden@live.nl>
2022-08-08 12:38:58 +0200

    (feat) implement post request and mutation hook for register POST endpoint

---
+++ client/src/helpers/fetch/user/useRegisterMutation.ts
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { NewUser } from "../../../components/Register/types/new-user.types";
+import { User } from "../../../types/user.types";
+
+const baseUrl = "http://localhost:5000"; // TODO: generalize this in an .env variable
+
+// TODO: generalize these fetch functions to prevent duplicating options a
+// million times.
+async function postRegister(newUser: NewUser) {
+	return (
+		await fetch(`${baseUrl}/user/register`, {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ newUser }),
+		})
+	).json();
+}
+
+export default function useRegisterMutation() {
+	return useMutation<User, any, NewUser>(["register"], async (newUser) =>
+		postRegister(newUser)
+	);
+}
