74f965105a0936128d348aec3d8bda75fcfa4998
Christiaan <christiaanseerden@live.nl>
2022-08-08 11:05:02 +0200

    (fix) rename import sql->sqlConnection

--- server/types/sql.types.ts
+++ server/types/sql.types.ts
@@ -1,5 +1,5 @@
-import { sql } from "../src/db/init";
+import { sqlConnection } from "../src/db/init";
 
-export type SQL = typeof sql;
+export type SQL = typeof sqlConnection;
 
 export type WithSQL<T> = T & { sql?: SQL };
