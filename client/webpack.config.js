d6c8964e8b42b3ac1f9fb61917cce2e3a1978559
Christiaan <christiaanseerden@live.nl>
2022-08-09 17:54:53 +0200

    (build) use splitChunks plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,69 +4,80 @@ const Dotenv = require("dotenv-webpack");
 const html = require("html-webpack-plugin");
 
 module.exports = {
-   devtool: "cheap-module-source-map",
-   entry: {
-      index: path.resolve(__dirname, "src/index.tsx"),
-   },
-   output: {
-      path: path.join(__dirname, "dist"),
-      filename: "bundle.js",
-      publicPath: "",
-   },
-   watchOptions: {
-      poll: 1000,
-   },
-   devServer: {
-      port: 3000,
-      host: "0.0.0.0",
-      historyApiFallback: true,
-      proxy: {
-         "/api": {
-            target: "http://localhost:5000",
-            pathRewrite: {
-               "^/api": "",
-            },
-            changeOrigin: true,
-         },
-      },
-      headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
-      },
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: {
-               loader: "ts-loader",
-            },
-         },
-         {
-            test: /\.scss$/,
-            use: [miniCss.loader, "css-loader", "sass-loader"],
-         },
-      ],
-   },
-   resolve: {
-      extensions: [".tsx", ".js", ".js", ".ts"],
-      modules: ["src", "node_modules"],
-      alias: {
-         components: path.resolve(__dirname, "src", "components/"),
-         hooks: path.resolve(__dirname, "src", "hooks/"),
-         helpers: path.resolve(__dirname, "src", "helpers/"),
-         state: path.resolve(__dirname, "src", "state/"),
-         types: path.resolve(__dirname, "src", "types/"),
-      },
-   },
-   plugins: [
-      new miniCss(),
-      new Dotenv(),
-      new html({
-         template: "./src/index.build.html",
-         filename: "index.build.html",
-      }),
-   ],
+	optimization: {
+		splitChunks: {
+			chunks: "async",
+			cacheGroups: {
+				defaultVendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10,
+					reuseExistingChunk: true,
+				},
+			},
+		},
+	},
+	devtool: "cheap-module-source-map",
+	entry: {
+		index: path.resolve(__dirname, "src/index.tsx"),
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js",
+		publicPath: "",
+	},
+	watchOptions: {
+		poll: 1000,
+	},
+	devServer: {
+		port: 3000,
+		host: "0.0.0.0",
+		historyApiFallback: true,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				pathRewrite: {
+					"^/api": "",
+				},
+				changeOrigin: true,
+			},
+		},
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "ts-loader",
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: [miniCss.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".js", ".js", ".ts"],
+		modules: ["src", "node_modules"],
+		alias: {
+			components: path.resolve(__dirname, "src", "components/"),
+			hooks: path.resolve(__dirname, "src", "hooks/"),
+			helpers: path.resolve(__dirname, "src", "helpers/"),
+			state: path.resolve(__dirname, "src", "state/"),
+			types: path.resolve(__dirname, "src", "types/"),
+		},
+	},
+	plugins: [
+		new miniCss(),
+		new Dotenv(),
+		new html({
+			template: "./src/index.build.html",
+			filename: "index.build.html",
+		}),
+	],
 };
