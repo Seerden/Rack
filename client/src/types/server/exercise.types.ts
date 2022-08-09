a86f4f23f609e2a1c6e255bf4a175a78d6cbbaf3
Christiaan <christiaanseerden@live.nl>
2022-08-09 16:42:36 +0200

    (type) copy server types to client

---
+++ client/src/types/server/exercise.types.ts
@@ -0,0 +1,37 @@
+export type NewWorkout = {
+   name: string;
+   description?: string;
+};
+
+/** Matches type of `workouts` table. */
+export type Workout = NewWorkout & {
+   workout_id: number;
+   user_id: number;
+};
+
+export enum WEIGHT_UNITS {
+   KG = "kg",
+   LBS = "lbs",
+}
+
+export type NewExercise = {
+   exercise_name: string;
+   sets: number;
+   reps: number;
+   starting_weight: number;
+   weight_progression: number;
+   weight_unit: WEIGHT_UNITS;
+};
+
+/** Matches type of `exercises` table. */
+export type Exercise = NewExercise & {
+   exercise_id: number;
+   workout_id: number;
+};
+
+/** This is the expected type of the object coming from the client. */
+export type WorkoutInput = NewWorkout & {
+   exercises: NewExercise[];
+};
+
+export type WorkoutWithExercises = Workout & { exercises: Exercise[] };
