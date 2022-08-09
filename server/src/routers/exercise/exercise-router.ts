794a8ab9e1b05f17ceac605b3ca5c493bf2a4f5c
Christiaan <christiaanseerden@live.nl>
2022-08-09 22:18:48 +0200

    (wip:feat) create workout POST route

--- server/src/routers/exercise/exercise-router.ts
+++ server/src/routers/exercise/exercise-router.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import { WorkoutInput } from "../../../types/exercise.types";
+import { createWorkout } from "./create-workout";
 
 export const exerciseRouter = Router({ mergeParams: true });
 
-exerciseRouter.post("/workout", async (req, res) => {});
+exerciseRouter.post("/workout", async (req, res) => {
+   const newWorkout: WorkoutInput = req.body?.newWorkout;
+
+   // @ts-ignore -- FIXME: temporarily using static user_id since we don't have session authentication yet
+   const user_id = req.session?.user_id ?? 1;
+
+   const workoutWithExercises = await createWorkout({ newWorkout, user_id });
+
+   res.json(workoutWithExercises);
+});
