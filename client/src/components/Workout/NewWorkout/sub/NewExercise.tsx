3249bf5fe412804fac8a99f34c16010be091f04e
Christiaan <christiaanseerden@live.nl>
2022-08-09 02:38:32 +0200

    (feat) create CollapsedExercise subcomponent; implement state and setters related to NewExercise collapse functionality

--- client/src/components/Workout/NewWorkout/sub/NewExercise.tsx
+++ client/src/components/Workout/NewWorkout/sub/NewExercise.tsx
@@ -1,14 +1,40 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useMemo } from "react";
+import { useRecoilValue } from "recoil";
+import { NewExercise } from "../../../../types/exercise.types";
 import useNewExercise from "../hooks/useNewExercise";
 import * as S from "../NewWorkout.style";
+import { newWorkoutState, openIndexState } from "../state/new-workout-state";
+import CollapsedExercise from "./CollapsedExercise";
 
 type NewExerciseProps = {
 	index: number;
 	onChange?: ChangeEventHandler<HTMLInputElement>;
 };
 
+const defaultExercise: NewExercise = {
+	exercise: "",
+	reps: 0,
+	sets: 0,
+	starting_weight: 0,
+	weight_progression: 0,
+	weight_unit: "kg",
+};
+
 export default function NewExercise({ index, onChange }: NewExerciseProps) {
 	const { fields } = useNewExercise(index);
+	const newWorkout = useRecoilValue(newWorkoutState);
+	const exercise = useMemo(() => {
+		return newWorkout?.exercises[index] ?? defaultExercise;
+	}, [newWorkout]);
+
+	const openIdx = useRecoilValue(openIndexState);
+	const collapsed = useMemo(() => {
+		return openIdx !== index;
+	}, [openIdx]);
+
+	if (collapsed) {
+		return <CollapsedExercise index={index} {...exercise} />;
+	}
 
 	return (
 		<S.Fieldset>
@@ -18,6 +44,7 @@ export default function NewExercise({ index, onChange }: NewExerciseProps) {
 					type="text"
 					id={fields.exercise}
 					name={fields.exercise}
+					defaultValue={exercise.exercise}
 					onChange={(e) => onChange?.(e)}
 				/>
 			</S.Field>
@@ -29,6 +56,7 @@ export default function NewExercise({ index, onChange }: NewExerciseProps) {
 						type="text"
 						id={fields.starting_weight}
 						name={fields.starting_weight}
+						defaultValue={exercise.starting_weight}
 						onChange={(e) => onChange?.(e)}
 					/>
 					<S.Select>
@@ -47,6 +75,7 @@ export default function NewExercise({ index, onChange }: NewExerciseProps) {
 							id={fields.sets}
 							name={fields.sets}
 							type={"number"}
+							defaultValue={exercise.sets}
 							min={1}
 							onChange={(e) => onChange?.(e)}
 						/>
@@ -57,6 +86,7 @@ export default function NewExercise({ index, onChange }: NewExerciseProps) {
 						<S.Input
 							id={fields.reps}
 							name={fields.reps}
+							defaultValue={exercise.reps}
 							type={"number"}
 							min={1}
 							onChange={(e) => onChange?.(e)}
@@ -66,12 +96,13 @@ export default function NewExercise({ index, onChange }: NewExerciseProps) {
 			</S.Field>
 
 			<S.Field gridArea="progress">
-				<S.Label htmlFor={fields.progression}>Weight progression</S.Label>
+				<S.Label htmlFor={fields.weight_progression}>Weight progression</S.Label>
 				<S.InputWithSelect>
 					<S.Input
 						type="text"
-						id={fields.progression}
-						name={fields.progression}
+						defaultValue={exercise.weight_progression}
+						id={fields.weight_progression}
+						name={fields.weight_progression}
 						onChange={(e) => onChange?.(e)}
 					/>
 					<S.Select>
