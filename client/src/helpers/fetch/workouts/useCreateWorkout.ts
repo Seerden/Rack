73e68f0334acd18e399837e798056d880afd0cfe
Christiaan <christiaanseerden@live.nl>
2022-08-12 18:37:41 +0200

    (feat) create fetch function and mutation hook for workout POST functionality

---
+++ client/src/helpers/fetch/workouts/useCreateWorkout.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { WorkoutInput, WorkoutWithExercises } from "../../../types/server/exercise.types";
+import { baseUrl, postConfig } from "../fetch-constants";
+
+async function postWorkout(newWorkout: WorkoutInput) {
+	return (
+		await fetch(`${baseUrl}/exercise/workout`, {
+			...postConfig,
+			body: JSON.stringify({ newWorkout }),
+		})
+	).json();
+}
+
+export default function useCreateWorkout() {
+	return useMutation<WorkoutWithExercises, any, WorkoutInput>(
+		["post/workout"],
+		async (newWorkout) => postWorkout(newWorkout)
+	);
+}
