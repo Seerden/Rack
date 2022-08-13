bbba5a96deda26011e2a07eb0134e4728a05aa1d
Christiaan <christiaanseerden@live.nl>
2022-08-13 13:13:50 +0200

    (fix) fix type import location

--- server/src/routers/user/user-router.ts
+++ server/src/routers/user/user-router.ts
@@ -1,25 +1,25 @@
 import { Router } from "express";
-import { isNewUser } from "../../../types/user.types";
+import { isNewUser } from "../../../types/shared/user.types";
 import { userExists } from "./get-user";
 import { insertUser, parseNewUserForDatabase } from "./register";
 
 export const userRouter = Router({ mergeParams: true });
 
 userRouter.post("/register", async (req, res) => {
-   const newUser: any = req.body.newUser;
+	const newUser: any = req.body.newUser;
 
-   if (!isNewUser(newUser)) {
-      return res.status(400).json({ message: "Invalid `newUser` in request body." });
-   }
+	if (!isNewUser(newUser)) {
+		return res.status(400).json({ message: "Invalid `newUser` in request body." });
+	}
 
-   // This check is also done inside `insertUser`, but this way it's easiest to
-   // send the correct response type without complicating `insertUser`
-   if (await userExists({ username: newUser.username }))
-      return res.status(403).json({ message: "Username already taken." });
+	// This check is also done inside `insertUser`, but this way it's easiest to
+	// send the correct response type without complicating `insertUser`
+	if (await userExists({ username: newUser.username }))
+		return res.status(403).json({ message: "Username already taken." });
 
-   const insertedUser = await insertUser({
-      userInput: await parseNewUserForDatabase(newUser),
-   });
+	const insertedUser = await insertUser({
+		userInput: await parseNewUserForDatabase(newUser),
+	});
 
-   return res.status(201).json({ user: insertedUser });
+	return res.status(201).json({ user: insertedUser });
 });
