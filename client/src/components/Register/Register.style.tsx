d0b68a38ef75490b1a1fb5b960771a660135d6e8
Christiaan <christiaanseerden@live.nl>
2022-08-13 16:50:19 +0200

    (tweak:style) use SubmitButton and activeInputStyle

--- client/src/components/Register/Register.style.tsx
+++ client/src/components/Register/Register.style.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { inputStyle } from "../../helpers/theme/snippets/input";
+import { activeInputStyle, inputStyle } from "../../helpers/theme/snippets/input";
 import { pageStyle } from "../../helpers/theme/snippets/page";
-import { inputShadow } from "../../helpers/theme/snippets/shadow";
+import { SubmitButton } from "../../helpers/theme/snippets/SubmitButton";
 import { inputLabelStyle } from "../../helpers/theme/snippets/text";
 
 const elementWidth = "200px";
@@ -12,7 +12,6 @@ export const Form = styled.form`
 
 export const Label = styled.label`
 	display: flex;
-	flex-direction: column;
 
 	${inputLabelStyle};
 `;
@@ -22,49 +21,11 @@ export const Input = styled.input`
 
 	${inputStyle};
 
-	&:focus,
-	&:active {
-		${inputShadow};
-		transform: translateY(-2px);
-	}
+	${activeInputStyle};
 `;
 
-export const Button = styled.button`
+export const Button = styled(SubmitButton)`
 	width: ${elementWidth};
 
 	margin-top: 1rem;
-
-	display: flex;
-	justify-content: center;
-	padding: 0.52rem 0.98rem;
-
-	outline: none;
-	border: 2px solid #bbb;
-	background-color: inherit;
-
-	font-size: 0.94rem;
-	font-weight: 500;
-
-	&:disabled {
-		color: #bbb;
-	}
-
-	&:not(:disabled) {
-		border-color: #aaa;
-		box-shadow: 0 0 0.2rem 0 #eee;
-		border-radius: 4px;
-
-		&:hover,
-		&:active,
-		&:focus {
-			${inputShadow}
-			transform: translateY(-4px);
-		}
-
-		&:focus {
-			outline: 1px solid black;
-		}
-	}
-
-	transition: all 45ms ease-out;
 `;
