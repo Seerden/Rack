7372ee207ec720ecb8e48a30733bfc2b75284add
Christiaan <christiaanseerden@live.nl>
2022-08-13 12:52:02 +0200

    (feat) implement isValidNewWorkout helper; (tweak) allow exercises without weight_unit in isValidNewExercise

--- client/src/components/Workout/NewWorkout/helpers/validate.ts
+++ client/src/components/Workout/NewWorkout/helpers/validate.ts
@@ -1,14 +1,18 @@
-import { NewExercise } from "../../../../types/shared/exercise.types";
+import { NewExercise, WorkoutInput } from "../../../../types/shared/exercise.types";
 
-function isValidNewWorkout() {
-	return;
+export function isValidNewWorkout(workout: WorkoutInput) {
+	const { exercises, name } = workout;
+
+	return name?.length && exercises.every((e) => isValidNewExercise(e));
 }
 
-function isValidNewExercise(exercise: NewExercise) {
+export function isValidNewExercise(
+	exercise: NewExercise | Omit<NewExercise, "weight_unit">
+) {
 	const { exercise_name, reps, sets, starting_weight, weight_progression } = exercise;
 
 	return (
-		exercise_name.length &&
+		exercise_name.length > 0 &&
 		reps > 0 &&
 		sets > 0 &&
 		starting_weight > 0 &&
