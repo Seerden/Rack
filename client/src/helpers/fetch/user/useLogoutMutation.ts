e72623c4da0b29a88cc3e0db386e6dab57060b96
Christiaan <christiaanseerden@live.nl>
2022-08-13 16:14:26 +0200

    (tweak) destroy local user from logout mutation onSuccess

--- client/src/helpers/fetch/user/useLogoutMutation.ts
+++ client/src/helpers/fetch/user/useLogoutMutation.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { localUser } from "../../auth/user-storage";
 import { baseUrl, postConfig } from "../fetch-constants";
 
 async function postLogout() {
@@ -15,6 +16,7 @@ export default function useLogoutMutation() {
 	return useMutation(["logout"], async () => postLogout(), {
 		// Unset local user on successful logout.
 		onSuccess: () => {
+			localUser.destroy();
 			client.removeQueries(["me"]);
 		},
 	});
