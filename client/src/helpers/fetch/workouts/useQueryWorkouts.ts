f2c655eda27374c61752cbf4679e9cbafeb65e6b
Christiaan <christiaanseerden@live.nl>
2022-08-14 17:36:21 +0200

    (wip:feat) add fetch function and query hook to query workouts by user

---
+++ client/src/helpers/fetch/workouts/useQueryWorkouts.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import { Data } from "../../../types/fetch-data";
+import { WorkoutWithExercises } from "../../../types/shared/exercise.types";
+import { baseUrl } from "../fetch-constants";
+
+async function getWorkoutsByUser(user_id?: number) {
+	if (!user_id) return;
+
+	// TODO: add options (method, credentials)
+	return (await fetch(`${baseUrl}/workouts/user/${user_id}`)).json();
+}
+
+export default function useQueryWorkoutsByUser() {
+	const { currentUser } = useAuth();
+
+	return useQuery<Data<"workouts", WorkoutWithExercises[]>>(["workouts"], async () =>
+		getWorkoutsByUser(currentUser?.user_id)
+	);
+}
