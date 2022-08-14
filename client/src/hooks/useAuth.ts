5f29521da23c86220d712552fe03403d945dd500
Christiaan <christiaanseerden@live.nl>
2022-08-14 22:34:07 +0200

    (fix) get currentUser from correct query hook

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,10 +1,13 @@
 import useLoginMutation from "../helpers/fetch/user/useLoginMutation";
 import useLogoutMutation from "../helpers/fetch/user/useLogoutMutation";
+import useMeQuery from "../helpers/fetch/user/useMeQuery";
 
 export default function useAuth() {
-	const { mutate: login, data } = useLoginMutation();
-	const currentUser = data?.user;
+	const { mutate: login } = useLoginMutation();
 	const { mutate: logout } = useLogoutMutation();
+
+	const { data } = useMeQuery();
+	const currentUser = data?.user;
 
 	const isLoggedIn = !!currentUser;
 
