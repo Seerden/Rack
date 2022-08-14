0ef2379f766eb8099fcae6a10825e8b140a7fa56
Christiaan <christiaanseerden@live.nl>
2022-08-15 00:58:45 +0200

    (feat) on useQueryWorkoutsByUser success, populate individual workouts in query cache

--- client/src/helpers/fetch/workouts/useQueryWorkouts.ts
+++ client/src/helpers/fetch/workouts/useQueryWorkouts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import { Data } from "../../../types/fetch-data";
 import { WorkoutWithExercises } from "../../../types/shared/exercise.types";
@@ -17,8 +17,20 @@ async function getWorkoutsByUser(user_id?: number) {
 
 export default function useQueryWorkoutsByUser() {
 	const { currentUser } = useAuth();
+	const client = useQueryClient();
 
-	return useQuery<Data<"workouts", WorkoutWithExercises[]>>(["workouts"], async () =>
-		getWorkoutsByUser(currentUser?.user_id)
+	return useQuery<Data<"workouts", WorkoutWithExercises[]>>(
+		["workouts"],
+		async () => getWorkoutsByUser(currentUser?.user_id),
+		{
+			// Populate ['workout', workout_id] entries in the query cache on success.
+			onSuccess: ({ workouts }) => {
+				if (!workouts.length) return;
+
+				for (const workout of workouts) {
+					client.setQueryData(["workout", workout.workout_id], workout);
+				}
+			},
+		}
 	);
 }
