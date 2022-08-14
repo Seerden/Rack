532b895b9579a13e308e8b6ab86073154d0dffe3
Christiaan <christiaanseerden@live.nl>
2022-08-14 17:33:04 +0200

    (feat) add GET endpoint to query workouts by user

--- server/src/routers/exercise/exercise-router.ts
+++ server/src/routers/exercise/exercise-router.ts
@@ -1,16 +1,25 @@
 import { Router } from "express";
 import { WorkoutInput } from "../../../types/shared/exercise.types";
 import { createWorkout } from "./create-workout";
+import { queryWorkoutsByUser } from "./query-workouts";
 
 export const exerciseRouter = Router({ mergeParams: true });
 
 exerciseRouter.post("/workout", async (req, res) => {
-   const newWorkout: WorkoutInput = req.body?.newWorkout;
+	const newWorkout: WorkoutInput = req.body?.newWorkout;
 
-   // @ts-ignore -- FIXME: temporarily using static user_id since we don't have session authentication yet
-   const user_id = req.session?.user_id ?? 1;
+	// @ts-ignore -- FIXME: temporarily using static user_id since we don't have session authentication yet
+	const user_id = req.session?.user_id ?? 1;
 
-   const workoutWithExercises = await createWorkout({ newWorkout, user_id });
+	const workoutWithExercises = await createWorkout({ newWorkout, user_id });
 
-   res.json(workoutWithExercises);
+	res.json(workoutWithExercises);
+});
+
+exerciseRouter.get("/workouts/user/:user_id/", async (req, res) => {
+	// TODO: any user can get data for any user like this. Make sure to implement
+	// `protected` middleware before pushing this live.
+	const user_id = +req.params.user_id;
+
+	res.json({ workouts: await queryWorkoutsByUser({ user_id }) });
 });
