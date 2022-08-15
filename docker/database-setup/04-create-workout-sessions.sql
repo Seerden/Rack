c9519375d2cd6df8dc867e878c68fff6166ced19
Christiaan <christiaanseerden@live.nl>
2022-08-15 13:31:40 +0200

    (tweak:db) add on delete constraints for workout sessions and entries

--- docker/database-setup/04-create-workout-sessions.sql
+++ docker/database-setup/04-create-workout-sessions.sql
@@ -1,6 +1,6 @@
 create table if not exists workout_sessions (
    workout_session_id serial primary key,
-   workout_id serial not null references workouts(workout_id),
+   workout_id serial not null references workouts(workout_id) on delete set null,
    created_at timestamp default now(),
    started_at timestamp not null,
    completed_at timestamp default now(),
--- docker/database-setup/05-create-workout-session-entries.sql
+++ docker/database-setup/05-create-workout-session-entries.sql
@@ -1,5 +1,5 @@
 create table if not exists workout_session_entries (
-   workout_session_id serial not null references workout_sessions(workout_session_id),
+   workout_session_id serial not null references workout_sessions(workout_session_id) on delete set null,
    workout_entry_id serial primary key,
    exercise_id serial not null references exercises(exercise_id),
    weight numeric(6,2) not null,
