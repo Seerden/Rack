6804666ae9d2ef7237ef1c9424800f613c12eba2
Christiaan <christiaanseerden@live.nl>
2022-08-16 10:37:44 +0200

    (feat) implement useRestTimer hook

---
+++ client/src/components/WorkoutSession/hooks/useRestTimer.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useMemo, useState } from "react";
+
+export default function useRestTimer(targetMillis: number, initialOn?: boolean) {
+	const [on, setOn] = useState<boolean>(initialOn ?? false);
+	const [delay, setDelay] = useState<number>(17); // Millisecond update interval -- ~17ms translates to 60fps
+	const [millisElapsed, setMillisElapsed] = useState<number>(0);
+	const reset = () => setMillisElapsed(0);
+	const start = () => setOn(true);
+	const progressPercentage = useMemo(
+		() => +((100 * millisElapsed) / targetMillis).toFixed(1),
+		[millisElapsed]
+	);
+
+	useEffect(() => {
+		if (!on) return;
+
+		const id = setInterval(() => {
+			setMillisElapsed((cur) => cur + delay);
+		}, delay);
+
+		return () => clearInterval(id);
+	}, [delay, on]);
+
+	return {
+		setDelay,
+		progressPercentage,
+		millisElapsed,
+		start,
+		reset,
+	} as const;
+}
