c886a007c5cd9f8e9f8f45a4d042d83a91f97ee1
Christiaan <christiaanseerden@live.nl>
2022-08-16 17:13:42 +0200

    (tweak) remove unnecessary fragment; change default rest time

--- client/src/components/WorkoutSession/sub/RestTimer.tsx
+++ client/src/components/WorkoutSession/sub/RestTimer.tsx
@@ -1,7 +1,7 @@
 import useRestTimer from "../hooks/useRestTimer";
 import * as S from "./RestTimer.style";
 
-const defaultRestMillis = 3 * 5 * 1000;
+const defaultRestMillis = 3 * 60 * 1000;
 
 export default function RestTimer() {
 	const { progressPercentage, millisElapsed, start, reset } = useRestTimer(
@@ -15,54 +15,52 @@ export default function RestTimer() {
 	const dashOffset = Math.max(0, ((100 - progressPercentage) / 100) * dashes);
 
 	return (
-		<>
-			<S.SVG
-				width={size}
-				height={size}
-				strokeDasharray={dashes}
-				strokeDashoffset={dashOffset}
-			>
-				<S.Group>
-					<S.Circle
-						cx={size / 2}
-						cy={size / 2}
-						r={0.4 * size}
-						fill="#eee"
-						strokeWidth={Math.min(7, 1 + 6 * (progressPercentage / 100))}
-						stroke={
-							progressPercentage >= 100
-								? "forestgreen"
-								: `rgba(0,0,0, ${progressPercentage / 100})`
-						}
-						strokeLinecap="round"
-					/>
-					<S.Circle
-						cx={size / 2}
-						cy={size / 2}
-						r={0.35 * size}
-						fill="#f2f2f2"
-						strokeWidth={0}
-					/>
+		<S.SVG
+			width={size}
+			height={size}
+			strokeDasharray={dashes}
+			strokeDashoffset={dashOffset}
+		>
+			<S.Group>
+				<S.Circle
+					cx={size / 2}
+					cy={size / 2}
+					r={0.4 * size}
+					fill="#eee"
+					strokeWidth={Math.min(7, 1 + 6 * (progressPercentage / 100))}
+					stroke={
+						progressPercentage >= 100
+							? "forestgreen"
+							: `rgba(0,0,0, ${progressPercentage / 100})`
+					}
+					strokeLinecap="round"
+				/>
+				<S.Circle
+					cx={size / 2}
+					cy={size / 2}
+					r={0.35 * size}
+					fill="#f2f2f2"
+					strokeWidth={0}
+				/>
 
-					<S.Text
-						pastDue={progressPercentage >= 100}
-						x={size / 2}
-						y={0.5 * size}
-						dominantBaseline="middle"
-						textAnchor="middle"
-					>
-						{(millisElapsed / 1000).toFixed(0)}s
-					</S.Text>
-					<S.Text
-						x={size / 2}
-						y={0.65 * size}
-						dominantBaseline="middle"
-						textAnchor="middle"
-					>
-						({defaultRestMillis / 1000}s)
-					</S.Text>
-				</S.Group>
-			</S.SVG>
-		</>
+				<S.Text
+					pastDue={progressPercentage >= 100}
+					x={size / 2}
+					y={0.5 * size}
+					dominantBaseline="middle"
+					textAnchor="middle"
+				>
+					{(millisElapsed / 1000).toFixed(0)}s
+				</S.Text>
+				<S.Text
+					x={size / 2}
+					y={0.65 * size}
+					dominantBaseline="middle"
+					textAnchor="middle"
+				>
+					({defaultRestMillis / 1000}s)
+				</S.Text>
+			</S.Group>
+		</S.SVG>
 	);
 }
