0d51a12ef2b0398841fa0f2c5463f9c0128f3898
Christiaan <christiaanseerden@live.nl>
2022-08-16 10:37:59 +0200

    (wip:feat) implement rest timer subcomponent

---
+++ client/src/components/WorkoutSession/sub/RestTimer.style.tsx
@@ -0,0 +1,65 @@
+import styled, { css } from "styled-components";
+
+export const Timer = styled.div`
+	width: 250px;
+	height: 250px;
+	border-radius: 50%;
+	padding: 1rem;
+	display: flex;
+	align-items: center;
+`;
+
+export const Circle = styled.circle`
+	transition: fill 45ms linear;
+
+	&:nth-of-type(1) {
+		-webkit-filter: drop-shadow(-0.2rem 0rem 0.5rem #ccc);
+		filter: drop-shadow(-0.2rem 0rem 0.5rem #ccc);
+	}
+`;
+
+export const SVG = styled.svg`
+	transform: rotate(-90deg);
+	transform-origin: 50% 50%;
+`;
+
+export const Text = styled.text<{ pastDue?: boolean }>`
+	transform-origin: 50% 50%;
+
+	user-select: none;
+
+	&:nth-of-type(1) {
+		font-weight: 700;
+		font-size: 1.5rem;
+	}
+
+	&:nth-of-type(2) {
+		font-weight: 400;
+		font-size: 1rem;
+	}
+
+	${(p) =>
+		p.pastDue &&
+		css`
+			fill: forestgreen;
+		`}
+
+	transform: rotate(90deg);
+
+	transition: all 50ms linear;
+`;
+
+export const Group = styled.g`
+	&:hover {
+		${Circle} {
+			stroke-width: 5px;
+		}
+	}
+
+	transition: all 55ms linear;
+
+	foreignObject {
+		transform: rotate(90deg);
+		transform-origin: 50% 50%;
+	}
+`;
---
+++ client/src/components/WorkoutSession/sub/RestTimer.tsx
@@ -0,0 +1,68 @@
+import useRestTimer from "../hooks/useRestTimer";
+import * as S from "./RestTimer.style";
+
+const defaultRestMillis = 3 * 5 * 1000;
+
+export default function RestTimer() {
+	const { progressPercentage, millisElapsed, start, reset } = useRestTimer(
+		defaultRestMillis,
+		true
+	);
+
+	// SVG attributes -- TODO: put these somewhere else
+	const size = 200;
+	const dashes = 2 * Math.PI * 0.4 * size;
+	const dashOffset = Math.max(0, ((100 - progressPercentage) / 100) * dashes);
+
+	return (
+		<>
+			<S.SVG
+				width={size}
+				height={size}
+				strokeDasharray={dashes}
+				strokeDashoffset={dashOffset}
+			>
+				<S.Group>
+					<S.Circle
+						cx={size / 2}
+						cy={size / 2}
+						r={0.4 * size}
+						fill="#eee"
+						strokeWidth={Math.min(7, 1 + 6 * (progressPercentage / 100))}
+						stroke={
+							progressPercentage >= 100
+								? "forestgreen"
+								: `rgba(0,0,0, ${progressPercentage / 100})`
+						}
+						strokeLinecap="round"
+					/>
+					<S.Circle
+						cx={size / 2}
+						cy={size / 2}
+						r={0.35 * size}
+						fill="#f2f2f2"
+						strokeWidth={0}
+					/>
+
+					<S.Text
+						pastDue={progressPercentage >= 100}
+						x={size / 2}
+						y={0.5 * size}
+						dominantBaseline="middle"
+						textAnchor="middle"
+					>
+						{(millisElapsed / 1000).toFixed(0)}s
+					</S.Text>
+					<S.Text
+						x={size / 2}
+						y={0.65 * size}
+						dominantBaseline="middle"
+						textAnchor="middle"
+					>
+						({defaultRestMillis / 1000}s)
+					</S.Text>
+				</S.Group>
+			</S.SVG>
+		</>
+	);
+}
