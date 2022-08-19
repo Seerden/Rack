1d700de40dc7117f814062dd3b0c326833adc726
Christiaan <christiaanseerden@live.nl>
2022-08-19 16:51:29 +0200

    (tweak) enable queries by default

--- client/src/helpers/query-client.ts
+++ client/src/helpers/query-client.ts
@@ -1,12 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-   defaultOptions: {
-      queries: {
-         staleTime: 0,
-         refetchOnMount: true,
-         cacheTime: 5 * 60 * 1000, // 5 minutes
-         refetchOnWindowFocus: true,
-      },
-   },
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+			enabled: true,
+			refetchOnMount: true,
+			cacheTime: 5 * 60 * 1000, // 5 minutes
+			refetchOnWindowFocus: true,
+		},
+	},
 });
