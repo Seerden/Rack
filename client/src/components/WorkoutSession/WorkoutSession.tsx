6b153b0deca21386b4996d3f87296bca04f214fc
Christiaan <christiaanseerden@live.nl>
2022-08-21 12:50:20 +0200

    (feat:style) add AnimatePresence animation to WorkoutSession

--- client/src/components/WorkoutSession/WorkoutSession.tsx
+++ client/src/components/WorkoutSession/WorkoutSession.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+import { pageVariants } from "../../helpers/framer/variants/page-variants";
 import { Title } from "../../helpers/theme/snippets/Title";
 import useWorkoutSession from "./hooks/useWorkoutSession";
 import ActiveExercise from "./sub/ActiveExercise";
@@ -18,33 +20,45 @@ export default function WorkoutSession() {
 	if (!workout?.exercises || !activeExercise) return <></>;
 
 	return (
-		<S.Page>
-			<Title>{workout?.name} session</Title>
+		<>
+			{activeExercise && (
+				<S.Page
+					key="m.page"
+					as={motion.div}
+					variants={pageVariants}
+					initial="hidden"
+					animate="appear"
+					exit="exit"
+					style={{ overflow: "hidden" }}
+				>
+					<Title as={motion.h1}>{workout?.name} session</Title>
 
-			<RestTimer />
+					<RestTimer />
 
-			<SessionLog
-				workout={workout}
-				onClick={setActiveExerciseId}
-				activeExerciseId={activeExerciseId ?? 0}
-			/>
+					<SessionLog
+						workout={workout}
+						onClick={setActiveExerciseId}
+						activeExerciseId={activeExerciseId ?? 0}
+					/>
 
-			{allCompleted && (
-				<div>
-					You've done all your working sets! Are you ready to end this session?
-				</div>
+					{allCompleted && (
+						<div>
+							You've done all your working sets! Are you ready to end this session?
+						</div>
+					)}
+
+					{!allCompleted && !!activeExercise && (
+						<ActiveExercise
+							cycleIndex={cycleActiveIndex}
+							workout={workout}
+							e={activeExercise}
+							// TODO: consider adding weight_unit to workout in addition to
+							// including it in each exercise
+							weight_unit={workout.exercises[0].weight_unit}
+						/>
+					)}
+				</S.Page>
 			)}
-
-			{!allCompleted && activeExercise && (
-				<ActiveExercise
-					cycleIndex={cycleActiveIndex}
-					workout={workout}
-					e={activeExercise}
-					// TODO: consider adding weight_unit to workout in addition to
-					// including it in each exercise
-					weight_unit={workout.exercises[0].weight_unit}
-				/>
-			)}
-		</S.Page>
+		</>
 	);
 }
