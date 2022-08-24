b1150ce87c8704171caa18c4a116d71b3040d61d
Christiaan <christiaanseerden@live.nl>
2022-08-24 20:55:43 +0200

    (wip:feat) implement WorkoutCompleted and EndButton subcomponents in WorkoutSession -- logic not yet complete

--- client/src/components/WorkoutSession/WorkoutSession.style.tsx
+++ client/src/components/WorkoutSession/WorkoutSession.style.tsx
@@ -1,9 +1,25 @@
 import styled from "styled-components";
 import { pageStyle } from "../../helpers/theme/snippets/page";
+import { SubmitButton } from "../../helpers/theme/snippets/SubmitButton";
 
 export const Page = styled.section`
 	${pageStyle}
 
 	display: flex;
 	flex-direction: column;
+`;
+
+export const EndButton = styled(SubmitButton)`
+	max-width: max-content;
+	place-self: flex-end;
+	margin-top: 0.8rem;
+
+	border-color: #ccc;
+
+	&:focus,
+	&:hover {
+		transform: translateY(
+			-1px
+		) !important; // needs to be important to overwrite AnimatePresence
+	}
 `;
--- client/src/components/WorkoutSession/WorkoutSession.tsx
+++ client/src/components/WorkoutSession/WorkoutSession.tsx
@@ -1,10 +1,13 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { MouseEvent, useState } from "react";
 import { pageVariants } from "../../helpers/framer/variants/page-variants";
+import { scaleOutVariants } from "../../helpers/framer/variants/slide-variants";
 import { Title } from "../../helpers/theme/snippets/Title";
 import useWorkoutSession from "./hooks/useWorkoutSession";
 import ActiveExercise from "./sub/ActiveExercise";
 import RestTimer from "./sub/RestTimer";
 import SessionLog from "./sub/SessionLog";
+import WorkoutCompleted from "./sub/WorkoutCompleted";
 import * as S from "./WorkoutSession.style";
 
 export default function WorkoutSession() {
@@ -17,48 +20,67 @@ export default function WorkoutSession() {
 		allCompleted,
 	} = useWorkoutSession();
 
+	const [showFinalize, setShowFinalize] = useState<boolean>(false);
+
 	if (!workout?.exercises || !activeExercise) return <></>;
 
+	if (!activeExercise) return <></>;
+
 	return (
-		<>
-			{activeExercise && (
-				<S.Page
-					key="m.page"
-					as={motion.div}
-					variants={pageVariants}
-					initial="hidden"
-					animate="appear"
-					exit="exit"
-					style={{ overflow: "hidden" }}
-				>
-					<Title as={motion.h1}>{workout?.name} session</Title>
+		<S.Page
+			key="m.page"
+			as={motion.div}
+			variants={pageVariants}
+			initial="hidden"
+			animate="appear"
+			exit="exit"
+			style={{ overflow: "hidden" }}
+		>
+			<Title as={motion.h1}>{workout?.name} session</Title>
 
-					<RestTimer />
+			<RestTimer />
 
-					<SessionLog
-						workout={workout}
-						onClick={setActiveExerciseId}
-						activeExerciseId={activeExerciseId ?? 0}
-					/>
-
-					{allCompleted && (
-						<div>
-							You've done all your working sets! Are you ready to end this session?
-						</div>
-					)}
-
-					{!allCompleted && !!activeExercise && (
-						<ActiveExercise
-							cycleIndex={cycleActiveIndex}
-							workout={workout}
-							e={activeExercise}
-							// TODO: consider adding weight_unit to workout in addition to
-							// including it in each exercise
-							weight_unit={workout.exercises[0].weight_unit}
-						/>
-					)}
-				</S.Page>
+			{!showFinalize && (
+				<SessionLog
+					workout={workout}
+					onClick={setActiveExerciseId}
+					activeExerciseId={activeExerciseId ?? 0}
+				/>
 			)}
-		</>
+
+			{showFinalize && <div></div>}
+
+			{!showFinalize && !!activeExercise && (
+				<ActiveExercise
+					cycleIndex={cycleActiveIndex}
+					workout={workout}
+					e={activeExercise}
+					// TODO: consider adding weight_unit to workout in addition to
+					// including it in each exercise
+					weight_unit={workout.exercises[0].weight_unit}
+				/>
+			)}
+
+			{allCompleted && (
+				<AnimatePresence mode="sync" key="m.WorkoutSession">
+					<WorkoutCompleted />
+					<AnimatePresence key="m.PresendEndButton">
+						<S.EndButton
+							key="m.endButton"
+							as={motion.button}
+							variants={scaleOutVariants}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							onClick={(e: MouseEvent<HTMLButtonElement>) => {
+								e.preventDefault();
+							}}
+						>
+							Complete session
+						</S.EndButton>
+					</AnimatePresence>
+				</AnimatePresence>
+			)}
+		</S.Page>
 	);
 }
