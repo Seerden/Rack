e7f8625c3a67e82fb97f65b1acc0c9709dc4599e
Christiaan <christiaanseerden@live.nl>
2022-08-26 21:18:11 +0200

    (feat) implement handleSubmit that parses, mutates, redirects on completion of workout session

--- client/src/components/WorkoutSession/hooks/useWorkoutSession.tsx
+++ client/src/components/WorkoutSession/hooks/useWorkoutSession.tsx
@@ -1,15 +1,22 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useParams } from "react-router";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 import { useRecoilValue } from "recoil";
+import useCreateWorkoutSession from "../../../helpers/fetch/workouts/useCreateWorkoutSession";
 import { useQuerySuggestedWorkout } from "../../../helpers/fetch/workouts/useQuerySuggestedWorkout";
 import { useQueryWorkoutById } from "../../../helpers/fetch/workouts/useQueryWorkoutById";
 import { ID } from "../../../types/shared/id.types";
+import { WorkoutSessionWithEntriesInput } from "../../../types/shared/session.types";
+import { parseSessionEntries } from "../helpers/parse-session-entries";
 import { activeWorkoutState, sessionEntriesState } from "../state/workout-state";
 
 export default function useWorkoutSession() {
 	const params = useParams();
+	const navigate = useNavigate();
 	const workout_id = +params.workout_id!;
 
+	const startDate = useRef(new Date());
+
+	const { mutate } = useCreateWorkoutSession();
 	const { data } = useQueryWorkoutById(workout_id);
 	const workout = useMemo(() => {
 		return data?.workout;
@@ -44,6 +51,27 @@ export default function useWorkoutSession() {
 	const allCompleted = session?.length && completedExercises.length === session.length;
 	const [activeExerciseId, setActiveExerciseId] = useState<number>();
 
+	const handleSubmit = useCallback(() => {
+		if (!workout || !allCompleted) return;
+
+		const sessionWithEntries: WorkoutSessionWithEntriesInput = {
+			workout_id,
+			completed_at: new Date(),
+			started_at: startDate.current,
+			created_at: new Date(),
+			entries: parseSessionEntries(sessionEntries, workout.weight_unit, session),
+		};
+
+		mutate(sessionWithEntries, {
+			onSuccess: () => {
+				// TODO: probably also want to optimistically set the received
+				// session in query cache, but we don't use this anywhere yet, so
+				// leave it be for now
+				navigate("/workouts");
+			},
+		});
+	}, [session, sessionEntries, workout]);
+
 	useEffect(() => {
 		if (session?.length) {
 			setActiveExerciseId(session[0].exercise_id);
@@ -59,6 +87,7 @@ export default function useWorkoutSession() {
 		setActiveExerciseId(session[newIndex].exercise_id);
 	}, [session, activeIndex, activeExerciseId]);
 
+	// TODO: dev-only state logging
 	useEffect(() => {
 		console.log({ sessionEntries, session });
 	}, [sessionEntries, session]);
@@ -70,5 +99,6 @@ export default function useWorkoutSession() {
 		activeExerciseId,
 		cycleActiveIndex,
 		allCompleted,
+		handleSubmit,
 	} as const;
 }
