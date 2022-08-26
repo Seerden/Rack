2d230f44a162b324182f64cb3af40ac1d6b183af
Christiaan <christiaanseerden@live.nl>
2022-08-26 23:35:18 +0200

    (chore) remove unnecessary whitespace in multiline comment

--- client/src/components/WorkoutSession/types/workout-state.types.ts
+++ client/src/components/WorkoutSession/types/workout-state.types.ts
@@ -2,8 +2,7 @@ import { SessionExercise } from "../../../types/shared/session.types";
 
 export type ActiveWorkout = SessionExercise[];
 
-/*  
-   This type ends up like
+/* This type ends up like
    {
       '2': { // this key represents an exercise_id
          '20kg': [{reps: 12, timestamp}], // the key represents a weight, the value is an array of {reps, timestamp}}
@@ -11,8 +10,7 @@ export type ActiveWorkout = SessionExercise[];
       '1': {
          '25kg': [{reps: 10, timestamp}, {...}]
       }
-   }  
-*/
+   } */
 export type SessionEntriesInput = Record<
 	number,
 	Record<string, { reps: number; timestamp: Date }[]>
