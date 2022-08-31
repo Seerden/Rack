87fcbe708ebcf2eb09f9dadda710f42d1bbd2258
Christiaan <christiaanseerden@live.nl>
2022-08-31 22:29:44 +0200

    (test) test render behavior and superficial functionality of NewWorkout

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,7 +1,12 @@
 import { rest } from "msw";
+import { baseUrl } from "../helpers/fetch/fetch-constants";
 
 export const handlers = [
 	rest.get("/test", (req, res, ctx) => {
 		return res(ctx.status(200), ctx.json({ message: "Hello, world" }));
+	}),
+	rest.post(`${baseUrl}/exercise/workout`, async (req, res, ctx) => {
+		console.log({ req: await req.json() });
+		return res(ctx.status(200), ctx.json({ message: "Success" }));
 	}),
 ];
