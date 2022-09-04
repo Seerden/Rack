003979f4ae9b29d10b3d8d2742ee26e00ef55ca7
Christiaan <christiaanseerden@live.nl>
2022-09-04 19:49:36 +0200

    (tweak) cascade workout_exercises on foreign key deletion

--- docker/database-setup/04-create-workout-exercises.sql
+++ docker/database-setup/04-create-workout-exercises.sql
@@ -1,6 +1,6 @@
 create table if not exists workout_exercises (
-   workout_id serial not null references workouts(workout_id),
-   exercise_id serial not null references exercises(exercise_id),
+   workout_id serial not null references workouts(workout_id) on delete cascade,
+   exercise_id serial not null references exercises(exercise_id) on delete cascade,
    unique(workout_id, exercise_id),
    primary key(workout_id, exercise_id)
 )
