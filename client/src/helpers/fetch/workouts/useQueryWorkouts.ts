5d1f39dd1884892cd1ee4b075597555d9f4dcb48
Christiaan Seerden <christiaanseerden@live.nl>
2024-11-07 00:32:48 +0100

    move query client instantiation outside of function scope to make it reactive

--- client/src/helpers/fetch/workouts/useQueryWorkouts.ts
+++ client/src/helpers/fetch/workouts/useQueryWorkouts.ts
@@ -1,12 +1,17 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useCallback } from "react";
 import useAuth from "../../../hooks/useAuth";
 import { Data } from "../../../types/fetch-data";
 import { WorkoutWithExercises } from "../../../types/shared/exercise.types";
 import { baseUrl } from "../fetch-constants";
 
 export default function useQueryWorkoutsByUser() {
+	const client = useQueryClient();
+
 	async function getWorkoutsByUser(user_id?: number) {
-		if (!user_id) return;
+		if (!user_id) {
+			return;
+		}
 
 		const response = await fetch(`${baseUrl}/exercise/workouts/user/${user_id}`, {
 			method: "GET",
@@ -16,7 +21,6 @@ export default function useQueryWorkoutsByUser() {
 		const data = await response.json();
 
 		// Populate ['workout', workout_id] entries in the query cache on success.
-		const client = useQueryClient();
 		if (data?.workouts) {
 			for (const workout of data.workouts) {
 				client.setQueryData(["workout", workout.workout_id], workout);
@@ -27,10 +31,14 @@ export default function useQueryWorkoutsByUser() {
 	}
 
 	const { currentUser } = useAuth();
+	const queryFn = useCallback(
+		() => getWorkoutsByUser(currentUser?.user_id),
+		[currentUser]
+	);
 
 	return useQuery<Data<"workouts", WorkoutWithExercises[]>>({
 		queryKey: ["workouts"],
-		queryFn: () => getWorkoutsByUser(currentUser?.user_id),
+		queryFn,
 		enabled: !!currentUser?.user_id,
 	});
 }
